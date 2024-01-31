499134c7e1ebe2c789ad3974cf852491f2326420
Antonio Marquina <antastur@gmail.com>
2024-02-01 00:23:00 +0100

    borrado import

--- src/main/java/com/proyectocardio/proyectocardio/controllers/ClienteController.java
+++ src/main/java/com/proyectocardio/proyectocardio/controllers/ClienteController.java
@@ -12,9 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.proyectocardio.proyectocardio.models.Alumno;
+
 import com.proyectocardio.proyectocardio.models.Cliente;
-import com.proyectocardio.proyectocardio.models.Equipo;
 import com.proyectocardio.proyectocardio.services.IClienteService;
 
 @CrossOrigin(origins = "http://localhost:4200,http://localhost:8080")
